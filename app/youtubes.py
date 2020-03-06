5b47649b24e4987c098f9e4c1a91df9ae5eb8045
bryan heinz <bryan@bryanheinz.com>
2020-03-06 04:34:08 +0000

    .

--- app/youtubes.py
+++ app/youtubes.py
@@ -28,7 +28,7 @@ def dl(url):
     print("Downloading {}".format(url.decode('utf-8')))
     cmd = [
         '/usr/bin/python3', '/usr/local/bin/youtube-dl', '-w',
-        '-f', '(bestvideo[ext=mp4]+bestaudio[ext=m4a])',
+        '-f', '(best[ext=mp4]/best)',
         '-o', os.path.join(config['dl_path'], '%(title)s.%(ext)s'),
         url.decode('utf-8')
     ]
